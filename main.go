c04fe4ffab42750c3acb825dc82e9c21d480501f
Nathan JANCZEWSKI <nathan@janczewski.fr>
2019-11-29 23:32:27 +0100

    Better design for account activation

--- main.go
+++ main.go
@@ -61,8 +61,7 @@ func main() {
 	// the react fontend app
 	r.HandleFunc("/", routes.IndexRoute(prv))
 
-	fs := http.FileServer(http.Dir("assets"))
-	r.Handle("/assets", fs)
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 
 	authRoutes := r.PathPrefix("/auth").Subrouter().StrictSlash(false)
 	authRoutes.HandleFunc("/login", middleware.CombineMiddlewaresCT(prv, routes.LoginRoute(prv))).Methods("POST")
