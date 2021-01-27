74ad157d93ddbf37f87b19f5b4298c81395d685b
Nathan JANCZEWSKI <nathan@janczewski.fr>
2021-01-27 14:50:25 +0100

    Side menu responsive

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,9 +1,14 @@
 <template>
   <nav :id="id" class="nav">
-    <font-awesome-icon class="nav--button" icon="bars"/>
-    <img class="nav--logo" src="" alt="Scinna logo"/>
-    <div class="nav--links">
-      <img class="nav--links-logo" src="" alt="Scinna logo"/>
+    <div v-if="menuOpened" id="global_background" :class="menuOpened ? 'shown' : ''" @click="hideMenu"/>
+
+    <font-awesome-icon class="nav--button" icon="bars" @click="showMenu"/>
+
+    <img class="nav--logo" src="../assets/logo.png" alt="Scinna logo"/>
+
+    <div :class="'nav--links ' + (menuOpened ? 'shown' : '')">
+      <img class="nav--links-logo" src="../assets/logo.png" alt="Scinna logo"/>
+
       <router-link class="link" to="/">Home</router-link>
       <router-link class="link" to="/">Profile</router-link>
       <router-link class="link" to="/">Logout</router-link>
@@ -17,6 +22,25 @@ import Vue from 'vue';
 export default Vue.extend({
   name: 'Navbar',
   props: ['id'],
+  data() {
+    return {
+      menuOpened: false,
+    }
+  },
+  methods: {
+    showMenu: function () {
+      this.menuOpened = true;
+    },
+    hideMenu: function () {
+      this.menuOpened = false;
+    },
+  },
+  mounted() {
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > 600)
+        this.menuOpened = false;
+    });
+  }
 });
 </script>
 
@@ -25,7 +49,7 @@ export default Vue.extend({
 @import '../assets/Colors.scss';
 
 .nav {
-  background: red;
+  background: $background-darker;
   padding: .5em;
 
   display: flex;
@@ -34,11 +58,20 @@ export default Vue.extend({
   align-items: center;
 
   &--button {
+    display: none;
+    position: absolute;
+    left: 1em;
+    height: 100%;
+
     font-size: 2em;
+    margin-right: 1em;
+
+    cursor: pointer;
   }
 
   &--logo {
     margin: .25em;
+    height: 2em;
   }
 
   &--links {
@@ -48,7 +81,29 @@ export default Vue.extend({
 
     justify-content: right;
     align-items: center;
-    @media (max-width: $size-s) {
+
+    &-logo {
+      display: none;
+    }
+
+    .link {
+      margin: 0 .5em;
+      font-weight: bold;
+      color: inherit;
+      text-decoration: none;
+    }
+  }
+}
+
+@media (max-width: $size-s) {
+  .nav {
+    justify-content: center;
+
+    &--button {
+      display: block;
+    }
+
+    &--links {
       position: absolute;
       flex-direction: column;
       background: $background-lighter;
@@ -58,21 +113,50 @@ export default Vue.extend({
 
       top: 0;
       left: 0;
-    }
+      transform: translateX(-100%);
+      transition: transform .3s ease-in-out;
 
-    &-logo {
-      display: none;
-
-      @media (max-width: $size-s) {
+      &-logo {
         display: block;
+        height: 2em;
+        margin: 1em 0;
+      }
+
+      .link {
+        margin: .75em 0;
+        padding: .5em;
+        width: 100%;
+        text-align: center;
+        background: darken($background, 5%);
+
+        &:nth-last-child {
+          margin-top: auto;
+        }
+      }
+
+      &.shown {
+        transform: translateX(0);
       }
     }
+  }
+}
 
-    .link {
-      margin: 0 .5em;
-      font-weight: bold;
-      color: inherit;
-      text-decoration: none;
+#global_background {
+  display: none;
+  position: absolute;
+
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+
+  background: rgba(black, .5);
+
+  z-index: 998;
+
+  &.shown {
+    @media (max-width: $size-s) {
+      display: block;
     }
   }
 }
