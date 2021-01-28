aa5795b8c4641b37a2f4d79404ce14c86beebf9b
Nathan JANCZEWSKI <nathan@janczewski.fr>
2021-01-28 19:20:19 +0100

    Admin admin flag in the database

--- models/User.go
+++ models/User.go
@@ -5,6 +5,8 @@ type User struct {
 	Name   string `db:"user_name"`
 	Email  string `db:"user_email"`
 
+	IsAdmin bool `db:"user_admin"`
+
 	Password       string  `db:"user_password" json:"-"`
 	Validated      bool    `db:"validated" json:"-"`
 	ValidationCode *string `db:"validation_code" json:"-"`
@@ -20,16 +22,18 @@ func (u User) GenerateTable() string {
 	return `
 		CREATE TABLE SCINNA_USER
 		(
-			USER_ID         uuid PRIMARY KEY             default gen_random_uuid(),
-			USER_NAME       VARCHAR(30) UNIQUE  NOT NULL,
-			USER_EMAIL      VARCHAR(255) UNIQUE NOT NULL,
-			USER_PASSWORD   VARCHAR             NOT NULL,
+			USER_ID         uuid PRIMARY KEY              default gen_random_uuid(),
+			USER_NAME       VARCHAR(30)  UNIQUE  NOT NULL,
+			USER_EMAIL      VARCHAR(255) UNIQUE  NOT NULL,
+			USER_PASSWORD   VARCHAR              NOT NULL,
+ 
+			USER_ADMIN      BOOL                 NOT NULL DEFAULT FALSE,
 
-			INVITATION_CODE VARCHAR(10)                  DEFAULT NULL,
+			INVITATION_CODE VARCHAR(10)                   DEFAULT NULL,
 
-			VALIDATED       BOOL                NOT NULL DEFAULT FALSE,
-			VALIDATION_CODE VARCHAR             NULL     DEFAULT gen_random_uuid(),
-			REGISTERED_AT   TIMESTAMP                    DEFAULT NOW()
+			VALIDATED       BOOL                 NOT NULL DEFAULT FALSE,
+			VALIDATION_CODE VARCHAR              NULL     DEFAULT gen_random_uuid(),
+			REGISTERED_AT   TIMESTAMP                     DEFAULT NOW()
 		);
 	`
 }
