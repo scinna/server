1c47904b886fa25fc9bac0de58faa096181d764b
Nathan JANCZEWSKI <nathan@janczewski.fr>
2021-03-17 13:41:27 +0100

    Better UI for token revocations

--- frontend/src/components/account/Token.tsx
+++ frontend/src/components/account/Token.tsx
@@ -1,7 +1,9 @@
-import React                from 'react';
-import i18n                 from 'i18n-js';
+import React from 'react';
+import i18n from 'i18n-js';
 import {Token as TokenType} from '../../types/Token';
-import {displayDate}        from "../../utils/DateUtils";
+import {displayDate} from "../../utils/DateUtils";
+import {IconButton} from "@material-ui/core";
+import ExitToApp from '@material-ui/icons/ExitToApp';
 
 import styles from '../../assets/scss/_Token.module.scss';
 
@@ -9,15 +11,19 @@ type TokenProps = {
     token: TokenType;
 }
 
-export function Token({ token }: TokenProps) {
+export function Token({token}: TokenProps) {
     return <div className={styles.Token + " " + (token.RevokedAt !== null ? styles.Token__Revoked : "")}>
-        <p>{i18n.t('my_profile.tokens.logged_at')} {displayDate(token.CreatedAt)}</p>
-        <p>{i18n.t('my_profile.tokens.last_seen')}: {displayDate(token.LastSeen)}</p>
-        {
-            token.RevokedAt
+        <div className={styles.Token__Infos}>
+            <p>{i18n.t('my_profile.tokens.logged_at')} {displayDate(token.CreatedAt)}</p>
+            <p>{i18n.t('my_profile.tokens.last_seen')}: {displayDate(token.LastSeen)}</p>
+            {
+                token.RevokedAt
                 && <p>{i18n.t('my_profile.tokens.revoked_at')} {displayDate(token.RevokedAt)}</p>
-        }
-        <p>{token.LoginIP}</p>
-        <a href="#">Logout</a>
+            }
+            <p>{token.LoginIP}</p>
+        </div>
+        <IconButton color="primary" aria-label="logout picture" component="span" disabled={token.RevokedAt !== null}>
+            <ExitToApp/>
+        </IconButton>
     </div>;
 }
